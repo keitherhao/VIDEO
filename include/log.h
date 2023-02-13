ac082370cd9ba3d6f04a3e339be262b0e1751968
KeithMo <keitherhao@outlook.com>
2023-02-13 15:56:29 +0800

    增加关闭log的宏MY_LOG_ON

--- include/log.h
+++ include/log.h
@@ -1,11 +1,17 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
+#define MY_LOG_ON	1
+
+#if MY_LOG_ON
 #define DBG_PRINTF(fmt, args...)  \
 do\
 {\
     printf("<<File:%s  Line:%d  Function:%s>> ", __FILE__, __LINE__, __FUNCTION__);\
     printf(fmt, ##args);\
 }while(0)
+#else
+#define DBG_PRINTF(fmt, args...)	do{}while(0)
+#endif
 
 #endif
