9a360ae2758999395361f5b3ebe11fffe4bc1019
KeithMo <keitherhao@outlook.com>
2023-02-16 16:39:10 +0800

    add uvc_ko

---
+++ other/uvc_ko/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Feb 16 16:21:46 CST 2023"
+#define LINUX_COMPILE_BY "keith"
+#define LINUX_COMPILE_HOST "DESKTOP-OIM0UOD"
+#define LINUX_COMPILER "gcc version 8.2.1 20180802 (GNU Toolchain for the A-profile Architecture 8.2-2018-08 (arm-rel-8.23)) "
